55cab43b0b69a18c87052a16a3a927db4de218da
YanaEgorova <yanaegorova69.3@gmail.com>
2019-11-13 16:31:37 +0200

    +

---
+++ index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const refs = {
+  body: document.querySelector('body'),
+  btnStart: document.querySelector('button[data-action="start"'),
+  btnStop: document.querySelector('button[data-action="stop"'),
+}
+
+console.log(refs.btnStart);
+console.log(refs.btnStop);
+
+//console.log(refs.body);
+
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const backgroundColor = function (colors) {
+  const index = randomIntegerFromInterval(0, colors.length - 1);
+  for (let i = 0; i < colors.length; i += 1) {
+    if (i === index) {
+      console.log('i: ', i);
+      return colors[i]
+    }
+  }
+
+};
+
+const change = function (array) {
+  setInterval(() => {
+    refs.body.style.backgroundColor = backgroundColor(array);
+  }, 1000)
+
+}
+
+
+
+
+refs.btnStart.addEventListener('click', change(colors));
