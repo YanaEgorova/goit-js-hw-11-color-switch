9474f40ce59e3e37fd6ed23a662f5e1e3aafba53
YanaEgorova <yanaegorova69.3@gmail.com>
2019-11-13 17:05:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -21,35 +21,41 @@ const colors = [
 ];
 
 
+const changeBackground = {
+  isActive: false,
+  start() {
+    if (!this.isActive) {
+      this.isActive = true;
+      this.change = setInterval(() => {
+        const randomIntegerFromInterval = (min, max) => {
+          return Math.floor(Math.random() * (max - min + 1) + min);
+        };
 
+        const backgroundColor = function (colors) {
+          const index = randomIntegerFromInterval(0, colors.length - 1);
+          for (let i = 0; i < colors.length; i += 1) {
+            if (i === index) {
+              console.log('i: ', i);
+              return colors[i]
+            }
+          }
 
+        };
 
+        refs.body.style.backgroundColor = backgroundColor(colors);
+      }, 1000)
 
-const randomIntegerFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
-
-const backgroundColor = function (colors) {
-  const index = randomIntegerFromInterval(0, colors.length - 1);
-  for (let i = 0; i < colors.length; i += 1) {
-    if (i === index) {
-      console.log('i: ', i);
-      return colors[i]
     }
   }
-
-};
-
-const change = function (array) {
-  setInterval(() => {
-    refs.body.style.backgroundColor = backgroundColor(array);
-  }, 1000)
-
 }
 
 
 
 
-refs.btnStart.addEventListener('click', () => {
-  change(colors)
-});
+
+
+
+
+
+
+refs.btnStart.addEventListener('click', changeBackground.start.bind(changeBackground));
