35a7eb2f2bf4b2780917e0f6bce37eb2473a34ae
YanaEgorova <yanaegorova69.3@gmail.com>
2019-11-13 18:55:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-'use strict';
+"use strict";
 
 const refs = {
-  body: document.querySelector('body'),
-  btnStart: document.querySelector('button[data-action="start"'),
-  btnStop: document.querySelector('button[data-action="stop"'),
-}
+  body: document.querySelector("body"),
+  btnStart: document.querySelector('button[data-action="start"]'),
+  btnStop: document.querySelector('button[data-action="stop"]')
+};
 
 console.log(refs.btnStart);
 console.log(refs.btnStop);
@@ -12,19 +12,17 @@ console.log(refs.btnStop);
 //console.log(refs.body);
 
 const colors = [
-  '#FFFFFF',
-  '#2196F3',
-  '#4CAF50',
-  '#FF9800',
-  '#009688',
-  '#795548',
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548"
 ];
-
 
 const changeBackground = {
   isActive: false,
   start() {
-
     if (this.isActive) {
       return;
     }
@@ -35,30 +33,32 @@ const changeBackground = {
         return Math.floor(Math.random() * (max - min + 1) + min);
       };
 
-      const backgroundColor = function (colors) {
+      const backgroundColor = function(colors) {
         const index = randomIntegerFromInterval(0, colors.length - 1);
         for (let i = 0; i < colors.length; i += 1) {
           if (i === index) {
-            console.log('i: ', i);
-            return colors[i]
+            console.log("i: ", i);
+            return colors[i];
           }
         }
-
       };
 
       refs.body.style.backgroundColor = backgroundColor(colors);
-    }, 1000)
-
-
+    }, 1000);
   },
 
   stop() {
     clearInterval(this.changeId);
     this.isActive = false;
   }
-}
+};
 
+refs.btnStart.addEventListener(
+  "click",
+  changeBackground.start.bind(changeBackground)
+);
 
-refs.btnStart.addEventListener('click', changeBackground.start.bind(changeBackground));
-
-refs.btnStop.addEventListener('click', changeBackground.stop.bind(changeBackground));
+refs.btnStop.addEventListener(
+  "click",
+  changeBackground.stop.bind(changeBackground)
+);
