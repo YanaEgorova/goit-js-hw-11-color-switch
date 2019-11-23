03ddcca35b042c34ecc85b391b959109dfec7dcb
YanaEgorova <yanaegorova69.3@gmail.com>
2019-11-23 17:05:06 +0200

    +

--- index.js
+++ index.js
@@ -1,64 +1,53 @@
 "use strict";
 
-const refs = {
-  body: document.querySelector("body"),
-  btnStart: document.querySelector('button[data-action="start"]'),
-  btnStop: document.querySelector('button[data-action="stop"]')
-};
+class ChangeColors {
+  constructor({ isActive = false, colors = ["#fff", "#000"] }) {
+    this.isActive = isActive;
+    this.colors = [...colors];
+    this.refs = {
+      btnStart: document.querySelector('button[data-action="start"]'),
+      btnStop: document.querySelector('button[data-action="stop"]'),
+      body: document.querySelector("body")
+    };
 
-console.log(refs.btnStart);
-console.log(refs.btnStop);
+    this.bindEvents();
+  }
 
-//console.log(refs.body);
-
-const colors = [
-  "#FFFFFF",
-  "#2196F3",
-  "#4CAF50",
-  "#FF9800",
-  "#009688",
-  "#795548"
-];
-
-const changeBackground = {
-  isActive: false,
-  start() {
+  starting() {
     if (this.isActive) {
       return;
     }
 
     this.isActive = true;
-    this.changeId = setInterval(() => {
-      const randomIntegerFromInterval = (min, max) => {
-        return Math.floor(Math.random() * (max - min + 1) + min);
-      };
 
-      const backgroundColor = function(colors) {
-        const index = randomIntegerFromInterval(0, colors.length - 1);
-        for (let i = 0; i < colors.length; i += 1) {
-          if (i === index) {
-            console.log("i: ", i);
-            return colors[i];
-          }
-        }
-      };
+    this.timer = setInterval(() => {
+      const nowIdxColor = this.randomIntegerFromInterval(
+        0,
+        this.colors.length - 1
+      );
+      console.log("Index color: ", nowIdxColor);
 
-      refs.body.style.backgroundColor = backgroundColor(colors);
+      this.refs.body.style.backgroundColor = this.colors[nowIdxColor];
     }, 1000);
-  },
+  }
 
   stop() {
-    clearInterval(this.changeId);
+    clearInterval(this.timer);
     this.isActive = false;
+    console.log("STOP");
   }
-};
 
-refs.btnStart.addEventListener(
-  "click",
-  changeBackground.start.bind(changeBackground)
-);
+  bindEvents() {
+    this.refs.btnStart.addEventListener("click", this.starting.bind(this));
+    this.refs.btnStop.addEventListener("click", this.stop.bind(this));
+  }
 
-refs.btnStop.addEventListener(
-  "click",
-  changeBackground.stop.bind(changeBackground)
-);
+  randomIntegerFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+}
+
+const changeColors = new ChangeColors({
+  element: document.querySelector("body"),
+  colors: ["#FFFFFF", "#2196F3", "#4CAF50", "#FF9800", "#009688", "#795548"]
+});
